c6267abf1c76a037d2f8dd427842662a33ee1a81
DON EJEH1~ <donejeh@gmail.com>
2024-04-08 21:35:23 +0100

    StockTest

---
+++ tests/BlogPostTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BlogPostTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/StockTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+
+
+class StockTest extends KernelTestCase{
+
+    public function testItworks(){
+
+        $this->assertTrue(true);
+    }
+}

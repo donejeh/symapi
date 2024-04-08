ae68b0ab765013130020e2fd91b2f9ba6f91a264
DON EJEH1~ <donejeh@gmail.com>
2024-04-08 21:52:51 +0100

    DatabasePrimer added

--- tests/StockTest.php
+++ tests/StockTest.php
@@ -3,9 +3,19 @@
 namespace App\Tests;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-
-
 class StockTest extends KernelTestCase{
+
+    private $entityManager;
+
+    protected function setUp(): void{
+
+        $kernel = self::bootKernel();
+
+        DatabasePrimer::prime($kernel);
+
+        $this->entityManager = $kernel->getContainer()->get('doctrine')->getManager();
+
+    }
 
     public function testItworks(){
 
